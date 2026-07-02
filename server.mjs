45dc4e61e6c649286fd49610758b8ed93fa7b698
paperee57 <150361308@qq.com>
2026-07-02 17:49:26 +0800

    docx: 更新文章

--- server.mjs
+++ server.mjs
@@ -28,9 +28,9 @@ app.set('view engine', 'ejs')
 app.set('views', client)
 
 app.use((req, res, next) => {
-  res.setHeader('X-Content-Type-Options', 'nosniff')  
-  res.setHeader('X-Frame-Options', 'DENY')  
-  res.setHeader('X-XSS-Protection', '1; mode=block')  
+  // res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Frame-Options', 'DENY')
+  res.setHeader('X-XSS-Protection', '1; mode=block')
   res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin')
   res.setHeader('Permissions-Policy', 'camera=(), microphone=(), geolocation=()')
   next()
